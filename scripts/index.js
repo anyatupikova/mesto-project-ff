b4e15f3d6c7423835191f9cc1a7bde41eaecad36
Anya Tupikova <anyatupikova@yandex.ru>
2024-06-14 18:51:40 +0400

    первый запуск

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function generateCard (element, deleteCard) {
+  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  const resetButton = cardElement.querySelector('.card__delete-button');
+  resetButton.addEventListener('click', deleteCard);
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+  evt.target.closest('.places__item').remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (element) {
+  const cardElement = generateCard(element, deleteCard);
+  cardsContainer.append(cardElement);
+})
